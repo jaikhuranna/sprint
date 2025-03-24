997e46fc2559a97e2e91c142e7f2f247b2e59542
skylancer76 <pawanbulusu@gmail.com>
2025-03-24 16:36:42 +0530

    Feature: Home Page

---
+++ Sprint/View Controllers/HomePageViewController.swift
@@ -0,0 +1,22 @@
+//
+//  HomePageViewController.swift
+//  Sprint
+//
+//  Created by admin19 on 24/03/25.
+//
+
+import UIKit
+
+class HomePageViewController: UIViewController {
+
+    @IBOutlet weak var bgView: UIView!
+    @IBOutlet weak var addCollectionButton: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        bgView.backgroundColor = .clear
+    }
+    
+
+}
