15a41efe4699b4c7855d55a0d311f755dbf4886b
Jai Khurana <64320051+jaikhuranna@users.noreply.github.com>
2025-03-24 18:36:54 +0530

    add base datamodel

---
+++ Collections.swift
@@ -0,0 +1,14 @@
+//
+//  Collections.swift
+//  Sprint
+//
+//  Created by Jai Khurana on 24/03/25.
+//
+
+import Foundation
+
+class Collection {
+    var notes: [Note] = []
+}
+
+
---
+++ Data.swift
@@ -0,0 +1,13 @@
+//
+//  Data.swift
+//  Sprint
+//
+//  Created by Jai Khurana on 24/03/25.
+//
+
+import Foundation
+
+class Data : Codable
+{
+    static var collections : [Collection] = []
+}
